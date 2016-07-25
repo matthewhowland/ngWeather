43a1a32c8ce0790eba5d90bf6a495adbf4457a32
Matthew Howland <howlandm@gmail.com>
2016-07-25 18:52:00 -0400

    copied justins code to finish project

---
+++ public/js/controllers/HomeController.js
@@ -0,0 +1,18 @@
+(function(){
+  angular.module('ngWeather')
+        .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['$scope', 'WeatherService'];
+
+  function HomeController($scope, WeatherService){
+    $scope.updateHourly = updateHourly;
+    $scope.updateMinutely = updateMinutely;
+    $scope.updateDaily = updateDaily;
+
+    function updateHourly(latitude, longitude){
+      WeatherService.getHourlyData(latitude, longitude);
+    }
+    function updateMinutely(latitude, longitude){}
+    function updateDaily(latitude, longitude){}
+  }
+})();
---
+++ public/js/controllers/HourlyController.js
@@ -0,0 +1,11 @@
+(function(){
+  angular.module('ngWeather')
+          .controller('HourlyController', HourlyController);
+
+  HourlyController.$inject = [];
+
+  function HourlyController(){
+
+
+  }
+})();
